668b30be7e0c3aaa5226bd6558832d13f9ff44fc
heming-langrenn <heming_harnes@hotmail.com>
2024-02-11 21:58:35 +0100

    Bugfix #173

--- result_service_gui/views/resultat_edit.py
+++ result_service_gui/views/resultat_edit.py
@@ -141,8 +141,7 @@ class ResultatEdit(web.View):
                     if form['publish'] != "false":
                         res = await ResultAdapter().update_result_status(user["token"], form["race_id"], 2)  # type: ignore
                         informasjon = f"Resultat er publisert ({res}). " + informasjon
-                        race_round = str(form["race"])
-                        if race_round.endswith("FA") or race_round.endswith("F"):
+                        if "raceclass_results" in form.keys():
                             res = await RaceclassResultsService().create_raceclass_results(
                                 user["token"], event, valgt_runde["klasse"]
                             )  # type: ignore
