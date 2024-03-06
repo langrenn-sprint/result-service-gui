0ef8f16bf38b0c43094f0f415b1d2caf98baaa0c
heming-langrenn <heming_harnes@hotmail.com>
2024-03-06 09:27:20 +0100

    Clean-up & documentation #170

--- result_service_gui/views/start.py
+++ result_service_gui/views/start.py
@@ -45,6 +45,11 @@ class Start(web.View):
             except Exception:
                 valgt_klasse = ""  # noqa: F841
                 informasjon += "Velg klasse for å se startlister."
+            try:
+                valgt_runde = self.request.rel_url.query["runde"]
+            except Exception:
+                valgt_runde = ""  # noqa: F841
+
             raceclasses = await RaceclassesAdapter().get_raceclasses(
                 user["token"], event_id
             )
@@ -72,6 +77,14 @@ class Start(web.View):
                         races.append(race)
                 raceplan_summary = get_raceplan_summary(_tmp_races, raceclasses)
 
+            # filter on selected round
+            if valgt_runde:
+                filtered_races = []
+                for race in races:
+                    if race['round'] == valgt_runde:
+                        filtered_races.append(race)
+                races = filtered_races
+
             # sort start list by starting position
             for race in races:
                 if len(race["startliste"]) > 1:
@@ -90,6 +103,7 @@ class Start(web.View):
                     "event_id": event_id,
                     "informasjon": informasjon,
                     "valgt_klasse": valgt_klasse,
+                    "valgt_runde": valgt_runde,
                     "raceclasses": raceclasses,
                     "races": races,
                     "raceplan_summary": raceplan_summary,
